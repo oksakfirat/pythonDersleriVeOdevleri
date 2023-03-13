a2651860c08d979d6f079f26c59292dcd09bb4eb
Genel Kullanicilar <firatoksak23@gmail.com>
2023-03-13 12:44:24 +0300

    python commit

---
+++ ilk ders ödevleri/veriTipleri.py
@@ -0,0 +1,17 @@
+#eğitmen ad soyad veri tipi olarak str kullanılmıştır.
+egitmenAd="Halit"
+egitmenSoyad="kalaycı"
+
+#kategori ve eğitmen kısımında veriler list veri tipi kullanılmış.
+kategori=["yazılım","programlama","grafik tasarım"]
+egitmen=["Engin demiroğ","Halit kalaycı"]
+
+#kursa kayıtlı değilse kurs içeriği gösterme burda veri tipi olarak bool kullanılmış ve denetleme olarak if else kullanılmış 
+kursaKayitOl=False
+if kursaKayitOl:
+    print("Kurs içeriği")
+else:
+    print("Kursa kayıt ol")
+
+#Kurs ilerleme durumu numerik veri tipi(int,float) kullanılmış.
+kursIlerlemeDurumu=50
