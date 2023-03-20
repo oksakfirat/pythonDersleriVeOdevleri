35c7840cb3758be785cd214a8a756af3ba7d3a96
Genel Kullanicilar <firatoksak23@gmail.com>
2023-03-20 12:45:23 +0300

    odev4 commit

--- uc ve dort ders odevleri/sauce_test.py
+++ uc ve dort ders odevleri/sauce_test.py
@@ -10,34 +10,27 @@ class Test_Sauce_Otomasyonu:
     inputPassword=chromeDriver.find_element(By.ID,"password")
     loginButton=chromeDriver.find_element(By.XPATH,"//*[@id='login-button']")
     def emptyLogin(self):
-       loginButton=self.chromeDriver.find_element(By.XPATH,"//*[@id='login-button']")
-       loginButton.click()
+       self.loginButton.click()
        sleep(2)
        errorName=self.chromeDriver.find_element(By.XPATH,"/html/body/div[1]/div/div[2]/div[1]/div/div/form/div[3]/h3")
        resultlogin=errorName.text=="Epic sadface: Username is required"
        print(resultlogin)
 
     def emptyPassword(self):
-        inputName=self.chromeDriver.find_element(By.XPATH,"/html/body/div[1]/div/div[2]/div[1]/div/div/form/div[1]/input")
-        inputName.send_keys("firat")
-        loginButton=self.chromeDriver.find_element(By.XPATH,"//*[@id='login-button']")
-        loginButton.click()
+        self.inputName.send_keys("firat")
+        self.loginButton.click()
         errorPassword=self.chromeDriver.find_element(By.XPATH,"/html/body/div/div/div[2]/div[1]/div/div/form/div[3]/h3")
         resultPassword=errorPassword.text=="Epic sadface: Password is required"
         print(resultPassword)
     
     def userLocked(self):
-        inputName=self.chromeDriver.find_element(By.XPATH,"/html/body/div[1]/div/div[2]/div[1]/div/div/form/div[1]/input")
-        inputName.send_keys("locked_out_user")
-        inputPassword=self.chromeDriver.find_element(By.ID,"password")
-        inputPassword.send_keys("secret_sauce")
-        loginButton=self.chromeDriver.find_element(By.XPATH,"//*[@id='login-button']")
-        loginButton.click()
+        self.inputName.send_keys("locked_out_user")
+        self.inputPassword.send_keys("secret_sauce")
+        self.loginButton.click()
         errorLocked=self.chromeDriver.find_element(By.XPATH,"/html/body/div/div/div[2]/div[1]/div/div/form/div[3]/h3")
         print(errorLocked.text=="Epic sadface: Sorry, this user has been locked out.")
     def x_IconClose(self):
-        loginButton=self.chromeDriver.find_element(By.XPATH,"//*[@id='login-button']")
-        loginButton.click()
+        self.loginButton.click()
         sleep(2)
         xIconClose=self.chromeDriver.find_element(By.XPATH,"/html/body/div/div/div[2]/div[1]/div/div/form/div[3]/h3/button")
         xIconClose.click()
